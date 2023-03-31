eb0f8e0b0665c74b50fa62e79471801e577cf4bc
Miguel Lumapat <lamppot@protonmail.com>
2023-03-30 21:27:54 -0400

    Start using SQLModel

--- loansvc/main.py
+++ loansvc/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
+from loansvc import models
 
 import sqlite3
 
@@ -15,13 +15,6 @@ print('Database connection established.')
 def index():
     return {"message": "Welcome to my REST API!"}
 
-class CreateUserPayload(BaseModel):
-    name: str
-
-class User(BaseModel):
-    id: int
-    name: str
-
 # TODO: Put this in a new file
 @app.get("/users")
 def get_users():
@@ -30,8 +23,8 @@ def get_users():
     return {'users': users}
 
 @app.post("/user")
-def create_user(create_request: CreateUserPayload):
-    d = create_request.dict()
+def create_user(user: models.User):
+    d = user.dict()
 
     # TODO: Implement
     return d
--- utils/init_server.sh
+++ utils/init_server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 ./setup_db.sh
-uvicorn main:app --host 0.0.0.0 --port 8000
+uvicorn loansvc.main:app --host 0.0.0.0 --port 8000
